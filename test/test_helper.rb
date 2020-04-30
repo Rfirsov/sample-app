68b35c8aeaa7c523aa2adc80df023b44ef8b6ae9
Roman Firsov <rf@anadeainc.com>
2020-04-30 23:37:51 +0300

    Add integration tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,13 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require "minitest/reporters"
+Minitest::Reporters.use!
+
+class ActiveSupport::TestCase
+  fixtures :all
+  include ApplicationHelper
+end
 
 class ActionDispatch::IntegrationTest
 end
