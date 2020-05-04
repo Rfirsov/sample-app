faa7f50dd14f465ea01f679c0f4454385f1ba893
Roman Firsov <rf@anadeainc.com>
2020-05-04 23:43:02 +0300

    Add signup page

--- db/schema.rb
+++ db/schema.rb
@@ -18,8 +18,8 @@ ActiveRecord::Schema.define(version: 2020_05_01_153614) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "password_digest"
   end
 
